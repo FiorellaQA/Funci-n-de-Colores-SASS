08103bd6a00c6796ba60e003099d7270df48c523
FiorellaQA <fiorella_90_17@hotmail.com>
2017-06-28 11:35:54 -0500

    corrigiendo directorio

--- assets/js/app.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-$.getJSON('http://laboratoria.cuadra.co:9339/api/v1/students/', function (data) {
-	$.each(data, function (index,elemento) {
-		$('table').append('<tr><td>' + elemento.name + '</td><td><input type="checkbox"></td></tr>');
-	})
-});
